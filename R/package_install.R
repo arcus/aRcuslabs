03b5e862f320f9478aafbdf4fd50878ac0b472f2
rosemm <hartmanr1@chop.edu>
2024-05-17 11:28:39 -0400

    Remove test function

--- R/package_install.R
+++ R/package_install.R
@@ -33,11 +33,3 @@ require <- function(...) {
     base::require(...)
   }
 }
-
-#' Test function
-#'
-#' To protect patient data, internet access is disabled in your Arcus Lab while your data is mounted.
-#' @export
-test_function <- function(...) {
-  message("\nTest\n")
-}
